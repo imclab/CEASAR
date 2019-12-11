46b26546fdf5497e290aec9abc9e74c6e0ac1978
Chris Hart <chart@concord.org>
2019-12-11 16:53:38 -0500

    A new scene for AR builds that is just for the Earth view and includes movement sync

---
+++ Assets/Scripts/AR/PlaceInteractableEarth.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+[RequireComponent(typeof(ARTrackedImageManager))]
+public class PlaceInteractableEarth : MonoBehaviour
+{
+    ARTrackedImageManager m_TrackedImageManager;
+    public float scaleAdjust = 1.0f;
+    void Awake()
+    {
+        m_TrackedImageManager = GetComponent<ARTrackedImageManager>();
+    }
+
+    void OnEnable()
+    {
+        m_TrackedImageManager.trackedImagesChanged += OnTrackedImagesChanged;
+    }
+
+    void OnDisable()
+    {
+        m_TrackedImageManager.trackedImagesChanged -= OnTrackedImagesChanged;
+    }
+    void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
+    {
+        foreach (var trackedImage in eventArgs.added)
+        {
+            trackedImage.transform.localScale = new Vector3(scaleAdjust, scaleAdjust, scaleAdjust);
+            
+        }
+    }
+}
